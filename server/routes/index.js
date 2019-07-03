b00cefa913c9c399075d6692f59a06094917df1b
amehraj <amehraj360@gmail.com>
2019-07-03 23:39:22 +0600

    Added Redis Caching for User (List and Update Function)

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,23 +3,23 @@ import ProductFunctions from '../controllers/product';
 import OrderFunctions from '../controllers/order';
 
 export default (app) => {
-
+  
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the BookStore API!',
   }));
 
   app.post('/api/user', UserFunctions.signUp);
-  app.get('/api/userlist', UserFunctions.viewAllUsers);
+  app.get('/api/user', UserFunctions.viewAllUsers);
   app.put('/api/user/:userId', UserFunctions.modifyUser);
   app.delete('/api/user/:userId', UserFunctions.deleteUser);
 
   app.post('/api/product', ProductFunctions.addProduct);
-  app.get('/api/productlist', ProductFunctions.viewAllProducts);
+  app.get('/api/product', ProductFunctions.viewAllProducts);
   app.put('/api/product/:productId', ProductFunctions.modifyProduct);
   app.delete('/api/product/:productId', ProductFunctions.deleteProduct);
 
   app.post('/api/order', OrderFunctions.addOrder);
-  app.get('/api/orderlist', OrderFunctions.viewAllOrders);
+  app.get('/api/order', OrderFunctions.viewAllOrders);
   app.put('/api/order/:orderId', OrderFunctions.modifyOrder);
   app.delete('/api/order/:orderId', OrderFunctions.deleteOrder);
 
